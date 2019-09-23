14a9ead82b3b698a1508293bc3c64deddc298a55
Jan Kuzník <jan.z.kuznik@telia.no>
2019-09-23 17:01:56 +0200

    Removing lombok as the IDE usually needs special configuration for the Annotation Processors

--- src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/domain/Photo.java
+++ src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/domain/Photo.java
@@ -1,17 +1,31 @@
 package cz.zonky.interview.task.kuznik.zonkyloanslisting.domain;
 
-import lombok.Data;
-
-/**
- * @Data is a convenient lombok shortcut annotation that bundles the features of
- * @ToString, @EqualsAndHashCode, @Getter / @Setter and @RequiredArgsConstructor together.
- * More info on setting everything up in here https://www.baeldung.com/lombok-ide .
- */
-
-@Data
 public class Photo {
 
     private String name;
     private String url;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
