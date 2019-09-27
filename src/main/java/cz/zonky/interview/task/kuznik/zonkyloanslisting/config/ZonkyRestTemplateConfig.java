ce09ffbe5c33b489ccec1b9ee5cf384fed5997c8
Jan Kuzník <jan.z.kuznik@telia.no>
2019-09-27 13:21:04 +0200

    paging supported through X-Page and X-Size headers

--- src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/config/ZonkyRestTemplateConfig.java
+++ src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/config/ZonkyRestTemplateConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,13 +14,22 @@ import java.nio.charset.Charset;
 import java.time.Duration;
 
 @Configuration
-public class ZonkyRestTemplate {
+public class ZonkyRestTemplateConfig {
 
     @Value("${zonky.rest.connect.timeout.milliseconds:5000}")
     private Long connectTimeout;
 
     @Value("${zonky.rest.read.timeout.milliseconds:5000}")
     private Long readTimeout;
+
+    @Value("${zonky.api.marketplace.paging.page:0}")
+    private Integer pageHeaderName;
+
+    @Value("${zonky.api.marketplace.paging.size:20}")
+    private Integer sizeHeaderName;
+
+    private static final String X_PAGE_HEADER = "X-Page";
+    private static final String X_SIZE_HEADER = "X-Size";
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -27,5 +39,15 @@ public class ZonkyRestTemplate {
                 .build();
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
         return restTemplate;
+    }
+
+    @Bean
+    public HttpEntity<String> httpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set(X_PAGE_HEADER, String.valueOf(pageHeaderName));
+        headers.set(X_SIZE_HEADER, String.valueOf(sizeHeaderName));
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+        return entity;
     }
 }
--- src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/connectors/ZonkyRestConnector.java
+++ src/main/java/cz/zonky/interview/task/kuznik/zonkyloanslisting/connectors/ZonkyRestConnector.java
@@ -13,20 +13,19 @@ import java.util.List;
 @Component
 public class ZonkyRestConnector {
 
-    @Value("${zonky.api.marketplace.url}")
-    private String marketPlaceUrl;
-
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private HttpEntity<String> httpEntity;
+
+    @Value("${zonky.api.marketplace.url}")
+    private String marketPlaceUrl;
 
     public List<Loan> getAllLoans() {
         ResponseEntity<List<Loan>> response;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
-
-        response = restTemplate.exchange(marketPlaceUrl, HttpMethod.GET, entity, new ParameterizedTypeReference<List<Loan>>(){});
+        response = restTemplate.exchange(marketPlaceUrl, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Loan>>(){});
 
         return response.getStatusCode() == HttpStatus.OK ? response.getBody() : null;
     }
